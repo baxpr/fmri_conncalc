13848de8a33bb46726472eff3737089e9f93a490
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-10-23 23:49:57 -0500

    Compiled the matlab

--- bin/test_compiled_matlab.sh
+++ bin/test_compiled_matlab.sh
@@ -1,4 +1,26 @@
 
+# szhab
+sh run_fmri_conncalc.sh /usr/local/MATLAB/MATLAB_Runtime/v92 \
+magick_path /usr/bin \
+param_file params_SZhab.csv \
+wroi_file '' \
+roi_file ../INPUTS/roi.nii.gz \
+roiinfo_file '' \
+coregmat_file ../INPUTS/coreg_mat.txt \
+deffwd_file ../INPUTS/y_deffwd.nii.gz \
+ct1_file ../INPUTS/ct1.nii.gz \
+wgm_file ../INPUTS/wgm.nii.gz \
+wcseg_file ../INPUTS/wcseg.nii.gz \
+func_file ../INPUTS/fmri.nii.gz \
+project UNK_PROJ \
+subject UNK_SUBJ \
+session UNK_SESS \
+scan UNK_SCAN \
+out_dir ../OUTPUTS
+
+exit 0
+
+
 # jsins
 sh run_fmri_conncalc.sh /usr/local/MATLAB/MATLAB_Runtime/v92 \
 magick_path /usr/bin \
